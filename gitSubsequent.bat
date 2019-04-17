87461b488481bf914da48471b700628aa0026b6a
Shaurya <agarwal.lucky.shaurya11!gmail.com>
2019-04-17 15:46:43 +0530

    New Commit

--- gitSubsequent.bat
+++ gitSubsequent.bat
@@ -5,7 +5,7 @@ set isgit=%remote:~-4%
 if "%isgit%" NEQ ".git" (set remote=%remote%.git && set url=%remote%) else (set url=%remote:.git=%)
 git remote set-url origin %remote%
 git add .
-if "%2"=="" (set msg="Files Committed") else (set msg=%2)
+if "%2"=="" (set msg="New Commit") else (set msg=%2)
 git commit -m %msg%
 git pull origin master
 git push -u origin master
