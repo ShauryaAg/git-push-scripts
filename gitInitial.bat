08ab805aab2c893fbe608b5ec58a0f17124a8726
Shaurya <agarwal.lucky.shaurya11!gmail.com>
2019-04-20 14:01:29 +0530

    New Commit

--- gitInitial.bat
+++ gitInitial.bat
@@ -5,5 +5,6 @@ set isgit=%remote:~-4%
 if "%isgit%" NEQ ".git" (set remote=%remote%.git)
 git remote add origin %remote%
 git add .
+git rm --cached %0
 git commit -m "Files Commited"
 git push -u origin master
--- gitSubsequent.bat
+++ gitSubsequent.bat
@@ -5,6 +5,7 @@ set isgit=%remote:~-4%
 if "%isgit%" NEQ ".git" (set remote=%remote%.git && set url=%remote%) else (set url=%remote:.git=%)
 git remote set-url origin %remote%
 git add .
+git rm --cached %0
 if "%2"=="" (set msg="New Commit") else (set msg=%2)
 git commit -m %msg%
 git pull origin master
